adafbcbf8eccb15872cfe70ded19dd4947c13723
Gary Cantuche <garycantuche2000@gmail.com>
2024-04-03 18:45:52 +0200

    First Version Working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css'
+import Board from './components/board/Board'
 import Navigation from './components/navbar/navigation'
 
 function App () {
   return (
     <main className='App'>
       <Navigation />
+      <Board />
     </main>
   )
 }
