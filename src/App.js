d7f1863470af79f2f072c82ac18fb0dc02e4ae39
Gary Cantuche <garycantuche2000@gmail.com>
2024-04-03 13:49:08 +0200

    First commit, added basic dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Navigation from './components/navbar/navigation'
+
+function App () {
+  return (
+    <main className='App'>
+      <Navigation />
+    </main>
+  )
+}
+
+export default App
