53703bbfaa5a426d93ca3c8ef5f092edf7eea82d
Gary Cantuche <garycantuche2000@gmail.com>
2024-04-08 16:43:16 +0200

    Added footer and fixed stytles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Board from './components/board/Board'
+import Footer from './components/footer/Footer'
 import Navigation from './components/navbar/navigation'
 
 function App () {
   return (
-    <main className='w-full p-3 sm:w-3/4 grid gap-y-10 '>
-      <Navigation />
-      <Board />
+    <main className='w-full p-0 flex justify-center'>
+      <div className='sm:w-3/4 grid gap-y-10 p-3'>
+        <Navigation />
+        <Board />
+      </div>
+      <Footer />
     </main>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full text-center absolute bottom-0 bg-configuration-back p-4 text-configuration-buttons">
+        <p>Created by: Gary Cantuche, credits to Midudev to share this exercice, and to MonkeyType, the original game.</p>
+    </footer>
+  );
+}
+
+export default Footer;
